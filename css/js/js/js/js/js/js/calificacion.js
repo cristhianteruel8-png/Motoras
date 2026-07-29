1661e6c1e14a4e4a791a6ab98a183e1caae147f3
cristhianteruel8-png <cristhianteruel8@gmail.com>
2026-07-29 20:15:07 -0300

    Delete css/js/js/js/js/js/js directory

--- css/js/js/js/js/js/js/calificacion.js
+++
@@ -1,7 +0,0 @@
-function calificar() {
-  const estrellas = document.getElementById("estrellas").value;
-  const resultado = document.getElementById("calificacionResultado");
-
-  resultado.style.display = "block";
-  resultado.textContent = "¡Gracias por calificar el servicio con " + estrellas + "!";
-}
