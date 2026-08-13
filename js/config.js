f6c0db86b16cdda05f08362053d035297779e968
cristhianteruel8-png <cristhianteruel8@gmail.com>
2026-08-12 21:55:02 -0300

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,6 @@
+export const CONFIG = {
+  SUPABASE_URL: "https://TU_PROYECTO.supabase.co",
+  SUPABASE_ANON_KEY: "TU_ANON_KEY_AQUI",
+  PAYPAL_CLIENT_ID: "TU_PAYPAL_CLIENT_ID_AQUI",
+  BASE_URL: "https://www.motoras.com.ar"
+};
