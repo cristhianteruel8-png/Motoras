2ed18f08be05384489baf3390bfc5ec359886788
cristhianteruel8-png <cristhianteruel8@gmail.com>
2026-08-13 20:36:04 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 export const CONFIG = {
   SUPABASE_URL: "https://zeidclylnspvmqfnojtq.supabase.co",
-  SUPABASE_ANON_KEY: "PEGA_AQUI_TU_ANON_KEY_REAL_DE_SUPABASE",
+  SUPABASE_ANON_KEY: "sb_publishable_6-mFlgoKCYPnW6Pkv5McTw_imIjw3ZQ",
   PAYPAL_CLIENT_ID: "AbMb_RzOR5YErLs9s6hSejhGtwEiSN7itjKtXlv3CwL-EnouKFyoUmW3zhcQWU-Femg47NXNxGXfh2j3",
   BASE_URL: "https://www.motoras.com.ar"
 };
