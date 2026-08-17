6056085bfbfe1098b77c80baca939a61f2bd8df3
cristhianteruel8-png <cristhianteruel8@gmail.com>
2026-08-16 21:04:40 -0300

    Update diagnostico.js

--- api/diagnostico.js
+++ api/diagnostico.js
@@ -1,3 +1,5 @@
+export const config = { runtime: 'nodejs' };
+
 export default async function handler(req, res) {
   if (req.method !== 'POST') return res.status(405).json({error:'Method not allowed'});
   const { falla } = req.body;
